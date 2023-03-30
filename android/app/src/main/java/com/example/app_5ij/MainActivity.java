7433e7739f7f81da16b71f067437fb26b86fbb49
unrealbootpower <46008343+Rossi-Davide@users.noreply.github.com>
2023-03-30 17:56:08 +0200

    modello e misurazione

--- android/app/src/main/java/com/example/app_5ij/MainActivity.java
+++ android/app/src/main/java/com/example/app_5ij/MainActivity.java
@@ -5,34 +5,142 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.HashMap;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.icu.util.ULocale;
+import android.util.Log;
+
+import org.tensorflow.lite.Tensor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.label.Category;
+import org.tensorflow.lite.task.core.BaseOptions;
+import org.tensorflow.lite.task.vision.detector.Detection;
+import org.tensorflow.lite.task.vision.detector.ObjectDetector;
+import org.tensorflow.lite.task.vision.detector.ObjectDetector.ObjectDetectorOptions;
+
 public class MainActivity extends FlutterActivity {
   private static final String CHANNEL = "app.5i.lifeel.dev/tensorflow";
 
   @Override
   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-  super.configureFlutterEngine(flutterEngine);
+    super.configureFlutterEngine(flutterEngine);
     new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
         .setMethodCallHandler(
-          (call, result) -> {
-            // This method is invoked on the main thread.
-            // TODO
-            if (call.method.equals("getOcchio")) {
-              String prova = getOcchio();
+            (call, result) -> {
+              // This method is invoked on the main thread.
+              // TODO
+              if (call.method.equals("getOcchio")) {
+                String prova = getOcchio();
 
-              if (prova != null) {
-                result.success(prova);
+                if (prova != null) {
+                  result.success(prova);
+                } else {
+                  result.error("UNAVAILABLE", "Prediction not available", null);
+                }
               } else {
-                result.error("UNAVAILABLE", "Prediction not available", null);
+                result.notImplemented();
               }
-            } else {
-              result.notImplemented();
-            }
-          }
-        );
+            });
   }
 
+  private String debugPrint(List<Detection> results) {
+    String test = "";
 
-  private String getOcchio(){
-    return "arrived";
+    for (Detection obj : results) {
+
+      List<Category> category = obj.getCategories();
+
+      for (int i = 0; i < category.size(); i++) {
+        test += category.get(i).getLabel() + "|";
+
+      }
+
+      RectF box = obj.getBoundingBox();
+      test += box.left + "|" + box.top + "|" + box.right + "|" + box.bottom + "/" + "moneta" + "|" + 0 + "|" + 0 + "|"
+          + 0 + "|" + 0;
+    }
+
+    return test;
+  }
+
+  private Bitmap drawDetectionResult(Bitmap bitmap, List<Detection> detectionResults) {
+    Bitmap outputBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+    Canvas canvas = new Canvas(outputBitmap);
+    Paint pen = new Paint();
+    pen.setTextAlign(Paint.Align.LEFT);
+
+    for (Detection detection : detectionResults) {
+      // draw bounding box
+      pen.setColor(Color.RED);
+      pen.setStrokeWidth(4F);
+      pen.setStyle(Paint.Style.STROKE);
+      RectF box = detection.getBoundingBox();
+      canvas.drawRect(box, pen);
+    }
+    return outputBitmap;
+  }
+
+  private String getOcchio() {
+    try {
+
+      // Initialization
+      ObjectDetectorOptions options = ObjectDetectorOptions.builder()
+          .setBaseOptions(BaseOptions.builder().useGpu().build())
+          .setMaxResults(2)
+          .setScoreThreshold(0.4f)
+          .build();
+
+      Context context = getApplicationContext();
+
+      ObjectDetector objectDetector = ObjectDetector.createFromFileAndOptions(context, "android.tflite", options);
+
+      File mSaveBit = new File(context.getCacheDir(), "test/prova2.jpg");
+      String filePath = mSaveBit.getPath();
+      Bitmap imageBitmap = BitmapFactory.decodeFile(filePath);
+
+      TensorImage image = TensorImage.fromBitmap(imageBitmap);
+
+      List<Detection> results = objectDetector.detect(image);
+
+      String test = debugPrint(results);
+
+      Bitmap rectImage = drawDetectionResult(imageBitmap, results);
+
+      // create a file to write bitmap data
+      File f = new File(context.getCacheDir(), "rectImage.png");
+      f.createNewFile();
+
+      ByteArrayOutputStream bos = new ByteArrayOutputStream();
+      rectImage.compress(Bitmap.CompressFormat.PNG, 0, bos);
+      byte[] bitmapData = bos.toByteArray();
+
+      // write the bytes in file
+      FileOutputStream fos = new FileOutputStream(f);
+      fos.write(bitmapData);
+      fos.flush();
+      fos.close();
+
+      String path = context.getCacheDir() + "/rectImage.png";
+
+      return test;
+
+    } catch (Exception e) {
+      return e.getMessage();
+    }
+
   }
 }
--- lib/model_Manager.dart
+++ lib/model_Manager.dart
@@ -4,42 +4,141 @@ import 'package:better_open_file/better_open_file.dart';
 import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:flutter/services.dart'; 
+import 'package:flutter/services.dart';
 
-
-
-
-
-class GetPrediction{
-
+class GetPrediction {
   static var platform;
 
- 
-  GetPrediction() {
-    platform = MethodChannel('app.5i.lifeel.dev/tensorflow');
-    
-    _getPrediction();
+  double misurazione = 0;
+  String risultato = "";
+  double widthM = 22.25;
 
-    
+  double midpoint2(double x1, double x2) {
+    return ((x1 + x2) * 0.5);
   }
 
- 
+  double midpoint1(double y1, double y2) {
+    return ((y1 + y2) * 0.5);
+  }
+
+/*
+  double calculateDistanceInPixels(double x1, double y1, double x2, double y2, double resolution) {
+    double deltaX = x2 - x1;
+    double deltaY = y2 - y1;
+
+    double distanceInPixels = distance * resolution;
+    return distanceInPixels;
+  }
+*/
+  void misura(dynamic widthM, dynamic leftM, dynamic topM, dynamic rightM,
+      dynamic botM, dynamic leftA, dynamic topA, dynamic rightA, dynamic botA) {
+    double pixelsPerMetric = 0;
+    double tltrX = 0,
+        tltrY = 0,
+        blbrX = 0,
+        blbrY = 0,
+        tlblX = 0,
+        tlblY = 0,
+        trbrX = 0,
+        trbrY = 0;
+    tltrX = midpoint2(leftM, rightM);
+    tltrY = midpoint1(topM, topM);
+    blbrX = midpoint2(leftM, rightM);
+    blbrY = midpoint1(botM, botM);
+    tlblX = midpoint2(leftM, leftM);
+    tlblY = midpoint1(topM, botM);
+    trbrX = midpoint2(rightM, rightM);
+    trbrY = midpoint1(topM, botM);
+    double distanceX = (tltrY - blbrY);
+    double distanceY = (tlblX - trbrX);
+    double distanceM = (distanceX + distanceY) / 2;
+    pixelsPerMetric = distanceM / widthM;
+    double tltrXanguilla = 0,
+        tltrYanguilla = 0,
+        blbrXanguilla = 0,
+        blbrYanguilla = 0,
+        tlblXanguilla = 0,
+        tlblYanguilla = 0,
+        trbrXanguilla = 0,
+        trbrYanguilla = 0;
+    tltrXanguilla = midpoint2(leftA, rightA);
+    tltrYanguilla = midpoint1(topA, topA);
+    blbrXanguilla = midpoint2(leftA, rightA);
+    blbrYanguilla = midpoint1(botA, botA);
+    tlblXanguilla = midpoint2(leftA, leftA);
+    tlblYanguilla = midpoint1(topA, botA);
+    trbrXanguilla = midpoint2(rightA, rightA);
+    trbrYanguilla = midpoint1(topA, botA);
+    double distanceXanguilla = (tltrYanguilla - blbrYanguilla);
+    double distanceYanguilla = (tlblXanguilla - trbrXanguilla);
+    double dimAanguilla = distanceXanguilla / pixelsPerMetric;
+    double dimBanguilla = distanceYanguilla / pixelsPerMetric;
+
+    misurazione = (dimAanguilla + dimBanguilla) / 2;
+    if (misurazione >= 0.1 && misurazione < 6.6) {
+      risultato = "residente";
+    } else if (misurazione >= 6.6 && misurazione < 7) {
+      risultato = "migrante al 90%";
+    } else {
+      risultato = "migrante";
+    }
+  }
+
+  GetPrediction(double width) {
+    platform = MethodChannel('app.5i.lifeel.dev/tensorflow');
+    widthM = width;
+    _getPrediction();
+  }
 
   Future<void> _getPrediction() async {
     String prediction;
 
     try {
       final String result = await platform.invokeMethod('getOcchio');
+      List<String> split = result.split("/");
+      if ((split[0].contains("occhio") && split[1].contains("moneta")) ||
+          (split[0].contains("moneta") && split[1].contains("occhio"))) {
+        String occhio;
+        String moneta;
 
-      var a = 5;
+        if (split[0].contains("occhio")) {
+          occhio = split[0];
+          moneta = split[1];
+          List<String> splitOcchio = occhio.split("|");
+          List<String> splitMoneta = moneta.split("|");
+          misura(
+              widthM,
+              double.parse(splitMoneta[1]),
+              double.parse(splitMoneta[2]),
+              double.parse(splitMoneta[3]),
+              double.parse(splitMoneta[4]),
+              double.parse(splitOcchio[1]),
+              double.parse(splitOcchio[2]),
+              double.parse(splitOcchio[3]),
+              double.parse(splitOcchio[4]));
+        } else {
+          occhio = split[1];
+          moneta = split[0];
+          List<String> splitOcchio = occhio.split("|");
+          List<String> splitMoneta = moneta.split("|");
+          misura(
+              widthM,
+              double.parse(splitMoneta[1]),
+              double.parse(splitMoneta[2]),
+              double.parse(splitMoneta[3]),
+              double.parse(splitMoneta[4]),
+              double.parse(splitOcchio[1]),
+              double.parse(splitOcchio[2]),
+              double.parse(splitOcchio[3]),
+              double.parse(splitOcchio[4]));
+        }
+      } else {
+        throw new Exception("Non è presente occhio e/o moneta");
+      }
 
-      print(a);
       print(result);
-    
-    } on PlatformException catch (e) {
-      prediction = "Failed to get prediction";
+    } on Exception catch (e) {
+      prediction = e.toString();
     }
   }
-
-  
 }
